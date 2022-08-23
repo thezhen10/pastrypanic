6720a67b422474fceb281a312799759df45c02cc
theMogget <y.kaiyi@outlook.com>
2022-08-24 00:04:37 +0800

    initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Hello! The Pastry Panic (with cat) site is currently under construction. Watch this space!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
