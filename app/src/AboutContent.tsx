e235e9636959ef6667f3a7eba1aabe20431e9acf
theMogget <y.kaiyi@outlook.com>
2022-09-14 23:38:30 +0900

    add about content

--- app/src/AboutContent.tsx
+++ app/src/AboutContent.tsx
@@ -13,44 +13,28 @@ export const AboutContent = (): JSX.Element => {
       <Text>{t('para0')}</Text>
       <Image maxW="50%" src={about1} />
       <Text>
-        Pastry Panic (with cat) is an arcade style game with a physical
-        controller that experiments with physicalizing mechanics through
-        swapping inputs with distinct haptic feedback.
+        {t('para1')}
         <br />
         <br />
-        In this single player game, players must swap input sliders that
-        represent baking tools in a pastry factory to complete as many pastries
-        as possible in a time limit. There are 2 input slots, corresponding to
-        the left and right conveyor belts that pastries descend from, and 5
-        baking actions in the game.
+        {t('para2')}
       </Text>
       <Image maxW="60%" src={about2} />
       <Text>
-        Each action is executed through a slider with a distinct haptic and
-        motion; cutting with a single press, kneading by repeatedly mashing,
-        holding the slider down to fill, sprinkling by shaking a slider that can
-        get stuck if pushed down too far, and a knob that locks into place for
-        baking (just remember to remove it).
+        {t('para3')}
         <br />
         <br />
-        Incomplete pastries will appear along the belts, and each type of pastry
-        requires a different sequence of actions to complete. The specific
-        recipes for the pastries are contained in a physical recipe book that
-        players must browse.
+        {t('para4')}
         <br />
         <br />
-        Between juggling sliders, executing inputs, reading recipes, and
-        handling two pastries simultaneously, Pastry Panic presents a frantic
-        experience where players must consider both the screen and the physical
-        space around them for gameplay.
+        {t('para5')}
       </Text>
       <Image maxW="60%" src={about3} />
       <Text>
         <Link color="blue" href="https://gdconf.com/alt-ctrl-gdc" isExternal>
-          alt.ctrl.GDC 2022 Audience award winner
+          {t('altctrl')}
         </Link>
         <br />
-        Made with techniques developed in{' '}
+        {t('techniques')}{' '}
         <Link
           color="blue"
           href="https://clementzheng.info/Shape-Haptics"
@@ -59,11 +43,8 @@ export const AboutContent = (): JSX.Element => {
           Shape Haptics
         </Link>
       </Text>
-      <Text>Contact - pastrypanicwithcat@gmail.com</Text>
-      <Text fontSize={'1.5rem'}>
-        Designed and developed by Yong Zhen Zhou, with support from Clement
-        Zheng and the Keio-NUS CUTE Center
-      </Text>
+      <Text>{t('contact')}</Text>
+      <Text fontSize={'1.5rem'}>{t('footer')}</Text>
     </Box>
   )
 }
--- app/src/locales/en/en.translation.ts
+++ app/src/locales/en/en.translation.ts
@@ -11,6 +11,21 @@ export const en = {
   [namespaces.pages.about]: {
     para0:
       'An arcade style alt.ctrl game with a haptic and physical twist! Also comes with a lazy cat. Try to make as much pastry as possible with a controller that challenges your ability to manage multiple input sliders, each with different haptic feedback.',
+    para1:
+      'Pastry Panic (with cat) is an arcade style game with a physical mechanics through swapping inputs with distinct haptic feedback.',
+    para2:
+      'In this single player game, players must swap input sliders that represent baking tools in a pastry factory to complete as many pastries as possible in a time limit. There are 2 input slots, corresponding to the left and right conveyor belts that pastries descend from, and 5 baking actions in the game.',
+    para3:
+      'Each action is executed through a slider with a distinct haptic and motion; cutting with a single press, kneading by repeatedly mashing, holding the slider down to fill, sprinkling by shaking a slider that can get stuck if pushed down too far, and a knob that locks into place for baking (just remember to remove it).',
+    para4:
+      'Incomplete pastries will appear along the belts, and each type of pastry requires a different sequence of actions to complete. The specific recipes for the pastries are contained in a physical recipe book that players must browse.',
+    para5:
+      'Between juggling sliders, executing inputs, reading recipes, and handling two pastries simultaneously, Pastry Panic presents a frantic experience where players must consider both the screen and the physical space around them for gameplay.',
+    altctrl: 'alt.ctrl.GDC 2022 Audience Award Winner',
+    techniques: 'Made with techniques developed in ',
+    contact: 'Contact - pastrypanicwithcat@gmail.com',
+    footer:
+      'Designed and developed by Yong Zhen Zhou, with support from Clement Zheng and the Keio-NUS CUTE Center',
   },
   [namespaces.pages.howto]: {},
 }
